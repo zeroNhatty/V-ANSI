c7294eb0f7092edb8d2ad0b18553fa5e5e09e8d5
zeroNhatty <ntnesi01@gmail.com>
2026-07-06 11:52:14 +0300

    feat: basic argument funtionality

---
+++ src/include/values.h
@@ -0,0 +1,9 @@
+//
+// Created by zero on 7/6/26.
+//
+
+#ifndef V_ANSI_VALUES_H
+#define V_ANSI_VALUES_H
+
+char* helpCommand = "Help: everything will be here\n";
+#endif //V_ANSI_VALUES_H
--- src/main.c
+++ src/main.c
@@ -1,28 +1,60 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+#include "include/values.h"
+#include "include/colors.h"
 
-int main() {
-    int choice;
-    while (true) {
-        printf("\nEntry:\n1. About V-ANSI \n2. Video Player \n3. Test \n0. Exit\n Choice: ");
-        fflush(stdout);
-        scanf("%d", &choice);
-        switch (choice) {
-            case 1:
-                printf("Sth about me says project");
-                break;
-            case 2:
-                printf("vid player");
-                break;
-            case 3:
-                break;
-            case 0:
-                printf("Exiting...\n\n\n");
-                fflush(stdout);
-                return 0;
-            default:
-                printf("Wrong choice. Please try again.");
-                fflush(stdout);
-        }
+void findVideo(char* path) {
+    FILE *f;
+
+    f= fopen(path, "r");
+    if (!f) {
+        fprintf(stderr, "Can't open file %s\n", path);
+        return;
     }
+
+
+}
+
+void warnWrongUsage(char* msg) {
+    printf("%sWarning:\033[m %s!", YELLOW_ANSI_COLOR, msg );
+    printf("%s", helpCommand);
+}
+
+int main(const int argc, char* argv[]) {
+    char *argument = NULL;
+    switch (argc) {
+        case 1:
+            warnWrongUsage("arguments must be provided!");
+            return -1;
+        case 2:
+            argument = argv[1];
+            bool isHelp = strcmp(argument, "help") == 0;
+            if(isHelp) {
+                printf("%s", helpCommand);
+                return 0;
+            }
+            else {
+                warnWrongUsage("unknown arguments!");
+            }
+            break;
+        case 3:
+            argument = argv[1];
+            bool is_d_argument = strcmp(argument, "-d") == 0;
+            if(is_d_argument) findVideo(argv[2]);
+            else {
+                warnWrongUsage("unknown arguments!");
+            }
+            break;
+        default:
+            warnWrongUsage("too much number of arguments!");
+            break;
+    }
+    /*
+     * let's assume we have 3 flags
+     * -d to tell the program
+     * whatever comes next is the path for the media file
+     * help to get description of commands
+     * so we only strictly use -d <path> and help alone so a max of 3
+     */
 }
